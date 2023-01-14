d1f8bb2a78e492966c1448f7c34cba562708cf34
Karen <est.Kpaza@uml.edu.ni>
2023-01-13 19:32:20 -0600

    Agregadas tablas

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -31,3 +31,33 @@ CREATE TABLE `horario` (
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB;
+
+CREATE TABLE `materias` (
+	`id_materias` INT(11) NOT NULL AUTO_INCREMENT,
+	`nombre_materia` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id_materias`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `notas` (
+	`id_notas` INT(11) NOT NULL AUTO_INCREMENT,
+	`nombre_estudiante` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`carrera_estudiante` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`nombre_materia` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`nombre_docente` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id_notas`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+CREATE TABLE `administrador` (
+	`id_administrador` INT(10) NOT NULL AUTO_INCREMENT,
+	`nombre_administrador` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`clave_administrador` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id_administrador`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
