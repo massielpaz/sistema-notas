276a66aefec537dcf15e7b8f76c0383c4a48bda8
Karen <est.Kpaza@uml.edu.ni>
2023-01-15 20:18:59 -0600

    Agregada migracion de tabla "docente"

---
+++ database/migrations/2023_01_16_020518_tabla_docentes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create("docentes", function (Blueprint $table) {
+            $table->integer('id_docente')->autoIncrement();
+            $table->string('nombre_docente');
+            $table->string('titulo_docente');
+            $table->string('correo_docente');
+            $table->string('telefono_docente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('docentes');
+    }
+};
